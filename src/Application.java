c3be1f7cc9c5b69a9a5d890323bd0f0c1c5d9e63
prado <antonio.ti.celso@gmail.com>
2021-12-30 18:16:18 +0000

    Refactor: Atualizando os metodos de: Dev

--- src/Application.java
+++ src/Application.java
@@ -1,4 +1,6 @@
+import com.github.antonioticelso.dominio.Bootcamp;
 import com.github.antonioticelso.dominio.Curso;
+import com.github.antonioticelso.dominio.Dev;
 import com.github.antonioticelso.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -22,9 +24,33 @@ public class Application {
         mentoria.setDescricao("mentoria da linguagem Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Bootcamp de Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devTonhao = new Dev();
+        devTonhao.setNome("Tonhao");
+        devTonhao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Tonhao: " + devTonhao.getConteudosInscritos());
+        devTonhao.progredir();
+        devTonhao.progredir();
+        System.out.println("Conteudos Inscritos Tonhao: " + devTonhao.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos Tonhao: " + devTonhao.getConteudosConcluidos());
+        System.out.println("XP Tonhao: " + devTonhao.calcularTotalXp());
+
+        System.out.println("-------");
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos Camila: " + devCamila.getConteudosInscritos());
+        devCamila.progredir();
+        System.out.println("Conteudos Inscritos Camila: " + devCamila.getConteudosInscritos());
+        System.out.println("Conteudos Concluidos Camila: " + devCamila.getConteudosConcluidos());
+        System.out.println("XP Camila: " + devCamila.calcularTotalXp());
 
     }
 
